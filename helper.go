c36d9052164d6464260e09f3d41069cd3f09ebd1
yterajima <terra@e2esound.com>
2015-12-06 20:29:45 +0900

    first commit

---
+++ helper.go
@@ -0,0 +1,9 @@
+package webpage
+
+func removeExtraSpace(text string) string {
+
+}
+
+func removeDuplicativeLineBreak(text string) string {
+
+}
---
+++ webpage.go
@@ -0,0 +1,11 @@
+package webpage
+
+import {
+	"github.com/PuerkitoBio/goquery"
+}
+
+type WebPage []byte
+
+func (wp *WebPage) Title() string {
+
+}
